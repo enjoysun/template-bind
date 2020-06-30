7133defc6bf89b4409fe80acadec291850a1775e
myou <myoueva@gmail.com>
2020-06-30 10:48:57 +0800

    project init

---
+++ auto-word-web/src/main/java/com/myou/autoword/web/AutoWordWebApplication.java
@@ -0,0 +1,13 @@
+package com.myou.autoword.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoWordWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoWordWebApplication.class, args);
+    }
+
+}
---
+++ auto-word-web/src/test/java/com/myou/autoword/web/AutoWordWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myou.autoword.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoWordWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
